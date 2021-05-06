212f0e8adb549502cbd5d9413671a5b19e55cdb2
Jonathan Martinez <jonathanmtzbello@gmail.com>
2021-05-06 18:57:18 -0500

    Endpoint para obtener un Afiliado. Endpoint para modificar un Afiliado

--- controllers/afiliados.js
+++ controllers/afiliados.js
@@ -1,5 +1,4 @@
 const bcrypt = require('bcryptjs');
-const { Op } = require("sequelize");
 const generarJWT = require('../helpers/generar-jwt');
 const subirIamgen = require('../helpers/subir-imagen');
 const Afiliado = require('../models/Afiliado');
@@ -9,7 +8,14 @@ let obtenerAfiliados = async (req, res, next) => {
     res.status(200).send(afiliados);
 }
 
-let obtenerAfiliado = (req, res, next) =>{}
+let obtenerAfiliado = async (req, res, next) =>{
+    let {id} = req.params;
+    const usuario = await Afiliado.findByPk(id);
+    if (!usuario) {
+        return res.status(404).send('Usuario no encontrado');
+    } 
+    res.status(200).send(usuario);
+}
 
 let crearAfiliado = async (req, res, next) => {
     let {body} = req;
@@ -27,6 +33,7 @@ let crearAfiliado = async (req, res, next) => {
         return res.status(400).send('El campo email debe ser un correo válido');
     }
     try {
+        //Busca el campo de teléfono en BD
         const existeTelefono = await Afiliado.findOne({
             where : {
                 telefono : body.telefono
@@ -34,6 +41,7 @@ let crearAfiliado = async (req, res, next) => {
         });
         //Verifica que no exista un teléfono en la BD
         if(existeTelefono){ return res.status(400).send('Ya existe un usuario con el telefono ' + body.telefono)}
+        //Busca el campo de Email en BD
         const existeEmail = await Afiliado.findOne({
             where : {
                 email : body.email
@@ -44,7 +52,7 @@ let crearAfiliado = async (req, res, next) => {
         //Cifrar contraseña
         const salt = bcrypt.genSaltSync();
         body.password = bcrypt.hashSync(body.password, salt);
-        //Generar enlace
+        //Generar enlace - Falta
 
         //Guarda el usuario Afiliado
         const afiliado = new Afiliado(body);
@@ -57,15 +65,78 @@ let crearAfiliado = async (req, res, next) => {
         res.status(201).send({afiliado, token, postImagen});
     } catch (error) {
         console.log(error);
-        res.status(500).send('Error del sistema, intente de nuevo o comuníquese con un asesor');
+        res.status(500).send('Error del sistema, intente de nuevo más tarde o comuníquese con un asesor');
     }
 }
 
-let modificarAfiliado = (req, res, next) =>{}
+let modificarAfiliado = async (req, res, next) =>{
+    let {id} = req.params;
+    let {body} = req;
+    try {
+        const afiliado = await Afiliado.findByPk(id);
+        if (!afiliado) {
+            return res.status(404).send('No existe el usuario con el id: ' + id);
+        }
+        if(body.telefono){
+            //Busca el campo de teléfono en BD
+            const existeTelefono = await Afiliado.findOne({
+                where : {
+                    telefono : body.telefono
+                }
+            });
+            //Verifica que no exista un teléfono en la BD
+            if(existeTelefono){ return res.status(400).send('Ya existe un usuario con el telefono ' + body.telefono)}
+        }
+        if(body.email){
+            //Busca el campo de Email en BD
+            const existeEmail = await Afiliado.findOne({
+                where : {
+                    email : body.email
+                }
+            });
+            //Verifica que no exista un email en la BD
+            if(existeEmail){ return res.status(400).send('Ya existe un usuario con el correo ' + body.email)}
+        }
+        if(body.password){
+            //Cifrar contraseña
+            const salt = bcrypt.genSaltSync();
+            body.password = bcrypt.hashSync(body.password, salt);
+        }
+        let postImagen=null;
+        if(body.public_id){
+            //Genera firma de la imagen
+            postImagen = subirIamgen(body.public_id);
 
-let eliminarAfiliado = (req, res, next) =>{}
+        }
+        //Actualiza el usuario Afiliado
+        await afiliado.update(body);
+        //Generar token
+        const token = await generarJWT(afiliado.dataValues.email);
+        //Se envían los datos al usuario
+        res.status(201).send(postImagen ? {afiliado, token, postImagen} : {afiliado, token});
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Error del sistema, intente de nuevo más tarde o comuníquese con un asesor');
+    }
+}
+
+//Función que elimina un usuario, se tiene que verificar
+let eliminarAfiliado = async (req, res, next) =>{
+    let {id} = req.params;
+    const usuario = await Afiliado.findByPk(id);
+    if (!usuario) {
+        return res.status(404).send('Usuario no encontrado');
+    }
+    //Opción de eliminar físicamente usuario
+    usuario.destroy();
+    //Opción de eliminar lógicamente usuario - falta
+
+    res.status(200).send(`Usuario ${id} eliminado`);
+}
 
 module.exports = {
     obtenerAfiliados,
-    crearAfiliado
+    obtenerAfiliado,
+    crearAfiliado,
+    modificarAfiliado
 }
--- routes/afiliados.js
+++ routes/afiliados.js
@@ -1,16 +1,16 @@
 const router = require('express').Router();
 const {
   crearAfiliado,
+  obtenerAfiliado,
   obtenerAfiliados,
-//   obtenerAfiliado,
-//   modificarAfiliado,
+  modificarAfiliado
 //   eliminarAfiliado
 } = require('../controllers/afiliados')
 
 router.get('/', obtenerAfiliados)
-// router.get('/:id', obtenerAfiliado) //Nuevo endpoint con todos los detalles de Afiliado
+router.get('/:id', obtenerAfiliado) //Nuevo endpoint con todos los detalles de Afiliado
 router.post('/', crearAfiliado)
-// router.put('/:id', modificarAfiliado)
+router.put('/:id', modificarAfiliado)
 // router.delete('/:id', eliminarAfiliado)
 
 module.exports = router;
